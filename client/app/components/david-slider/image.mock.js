9ee83236f22c86e50c3d8999dce39e166dd6d437
unknown <dlorenzo.05@gmail.com>
2020-11-23 18:33:53 +0100

    image.mock.js file created, array of objects, ech object is an image with name, url and id

---
+++ client/app/components/david-slider/image.mock.js
@@ -0,0 +1,24 @@
+const images = [
+  {
+    name: 'mountains',
+    id: 0,
+    url: 'https://images.unsplash.com/photo-1601244207683-03c692c739ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80',
+  },
+  {
+    name: 'prague',
+    id: 1,
+    url: 'https://images.unsplash.com/photo-1576612981434-512939997682?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  },
+  {
+    name: 'city',
+    id: 2,
+    url: 'https://images.unsplash.com/photo-1551806580-e668327b0611?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+  },
+  {
+    name: 'tower',
+    id: 3,
+    url: 'https://images.unsplash.com/photo-1544763290-ea7d0cf38601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
+  },
+];
+
+export default images;
