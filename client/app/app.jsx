9b8eeaa7267f8483964e7c3bc0014f28ebc1cf22
unknown <dlorenzo.05@gmail.com>
2020-11-23 16:56:46 +0100

    app.jsx file created for rendering the slider

---
+++ client/app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import DavidSlider from './components/david-slider';
+
+import './reset.scss';
+
+const App = () => (
+  <div className="app">
+    <DavidSlider />
+  </div>
+);
+
+export default App;
